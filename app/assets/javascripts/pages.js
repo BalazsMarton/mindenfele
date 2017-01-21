52567fcd86493c67ef9e0e7d738ec51ad094808b
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-01-22 00:12:20 +0100

    index page redesign, background change

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,14 +6,12 @@ Turbolinks.enableProgressBar();
           CKEDITOR.replace($('.ckeditor').attr('id'));
       }
   });
- $(document).on('page:load', function(){
- 	$('#admin-sidebar').affix({offset: {top: 1} });
- });
  $(document).ready(function () {
  	$('.owl-carousel').owlCarousel({
  		items:1,
     	margin:10,
     	nav:true,
+    	navText : ["",""],
     	autoHeight:true
 
  	});
