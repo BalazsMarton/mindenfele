f0c3c01a825f968de5c53697a338d920dd262937
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-01-22 02:54:03 +0100

    index page beautyfy

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -14,4 +14,10 @@ Turbolinks.enableProgressBar();
     	navText : ["",""],
 
  	});
+
+if(window.matchMedia('(max-width: 767px)').matches) {
+	var bg = jQuery(".carousel-item");
+    bg.height(jQuery(window).height()*0.4);
+};
+
  });
